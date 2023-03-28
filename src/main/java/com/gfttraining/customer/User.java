e7467984ca043ff311931332d34bd8d0c96a3516
Sergi Perez <sergi.perez@gft.com>
2023-03-28 14:01:13 +0000

    lombok added

--- src/main/java/com/gfttraining/customer/User.java
+++ src/main/java/com/gfttraining/customer/User.java
@@ -4,18 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class User {
-	
+
 	@Id()
 	@GeneratedValue()
 	private int id;
-	
+
 	private String name;
 	private String lastname;
 	private String address;
 	private String paymentmethod;
-	
+
 	public User(int id, String name, String lastname, String address, String paymentmethod) {
 		this.id = id;
 		this.name = name;
@@ -26,46 +29,5 @@ public class User {
 
 	public User() {}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPaymentmethod() {
-		return paymentmethod;
-	}
-
-	public void setPaymentmethod(String paymentmethod) {
-		this.paymentmethod = paymentmethod;
-	}
-	
-	
 
 }
