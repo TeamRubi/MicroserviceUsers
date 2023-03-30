7ab730c5ce4b51a100631b3680ee6d2e42d0da0f
Sergi Perez <sergi.perez@gft.com>
2023-03-30 11:55:32 +0000

    createUser feature

--- MicroservicioTeamRubi/src/main/java/com/gfttraining/controller/UserController.java
+++ MicroservicioTeamRubi/src/main/java/com/gfttraining/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.gfttraining.controller;
 import java.util.List;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gfttraining.service.UserService;
 import com.gfttraining.user.User;
+
 @RestController
 public class UserController {
 
@@ -23,6 +32,16 @@ public class UserController {
 		return userService.findAll();
 	}
 
+	@GetMapping("/users/{id}")
+	public User GetUserById(@PathVariable int id){
+		return userService.findUserById(id);
+	}
+
+	@GetMapping("/users/name/{name}")
+	public User GetUserById(@PathVariable String name){
+		return userService.findUserByName(name);
+	}
+
 	@DeleteMapping("/users/{id}")
 	public ResponseEntity<Void> deleteUserById(@PathVariable int id){
 		userService.deleteUserById(id);
@@ -30,8 +49,8 @@ public class UserController {
 	}
 
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
-		return userService.createUser(user);
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
 	}
 
 }
--- MicroservicioTeamRubi/src/main/java/com/gfttraining/service/UserService.java
+++ MicroservicioTeamRubi/src/main/java/com/gfttraining/service/UserService.java
@@ -1,5 +1,9 @@
 package com.gfttraining.service;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,15 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
+	public User findUserById(Integer id){
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+	}
+
+	public User findUserByName(String name){
+		Optional<User> user = Optional.ofNullable(userRepository.findByName(name));
+		return user.get();
+	}
 
 	public void deleteUserById(Integer id) {
 		try {
@@ -25,7 +38,6 @@ public class UserService {
 	}
 
 	public User createUser(User user) {
-
 		return userRepository.save(user);
 	}
 
