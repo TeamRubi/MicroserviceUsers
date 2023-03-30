8c0f3ac88ae843c01ac92cf030f14b8e6e856306
Sergi Perez <sergi.perez@gft.com>
2023-03-30 11:58:57 +0000

    added exception handlers

---
+++ MicroservicioTeamRubi/src/main/java/com/gfttraining/exception/ExceptionResponse.java
@@ -0,0 +1,27 @@
+package com.gfttraining.exception;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+
+@Data
+public class ExceptionResponse {
+
+	private Date timestamp;
+	private String message;
+	private List<String> details;
+
+	public ExceptionResponse(Date timestamp, String message, List<String> details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public ExceptionResponse(String message, Date timestamp) {
+		this.message = message;
+		this.timestamp = timestamp;
+	}
+}
