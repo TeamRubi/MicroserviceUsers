db672295bac1b0612cfa6bae4fe4f56957e4c5ee
Sergi Perez <sergi.perez@gft.com>
2023-03-30 13:53:12 +0000

    solving error

--- MicroservicioTeamRubi/src/main/java/com/gfttraining/service/UserService.java
+++ MicroservicioTeamRubi/src/main/java/com/gfttraining/service/UserService.java
@@ -17,21 +17,25 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
-	
+
 	public User findUserById(Integer id){
 		Optional<User> user = userRepository.findById(id);
 		return user.get();
 	}
-	
+
 	public User findUserByName(String name){
 		Optional<User> user = Optional.ofNullable(userRepository.findByName(name));
 		return user.get();
+	}
+
 	public void saveUser(User user) {
 		userRepository.save(user);
 	}
+
 	public void saveAllUsers(List<User> usersList) {
 		userRepository.saveAll(usersList);
 	}
+
 	public void deleteAllUsers() {
 		userRepository.deleteAll();
 	}
@@ -45,7 +49,6 @@ public class UserService {
 	}
 
 	public User createUser(User user) {
-
 		return userRepository.save(user);
 	}
 
