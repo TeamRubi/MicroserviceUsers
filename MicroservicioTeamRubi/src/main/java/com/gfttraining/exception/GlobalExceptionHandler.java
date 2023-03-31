06eae8489f9a6431b0c6c215c1c029f74f051c70
Sergi Perez <sergi.perez@gft.com>
2023-03-31 06:20:36 +0000

    handling errors

--- MicroservicioTeamRubi/src/main/java/com/gfttraining/exception/GlobalExceptionHandler.java
+++ MicroservicioTeamRubi/src/main/java/com/gfttraining/exception/GlobalExceptionHandler.java
@@ -7,17 +7,56 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-	
+
 	@ExceptionHandler
 	public ResponseEntity<ExceptionResponse> handlerException(EntityNotFoundException exception,WebRequest req){
 		ExceptionResponse res = new ExceptionResponse(new Date(),exception.getMessage(),null);
-		
+
 		return new ResponseEntity<ExceptionResponse>(res, HttpStatus.NOT_FOUND);
 	};
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+
+		List<String> errors = new ArrayList<>();
+
+		for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+			errors.add(violation.getPropertyPath() + ": " + violation.getMessage());
+		}
+
+		ExceptionResponse res = new ExceptionResponse(new Date(),"constraint violation", errors);
+		return new ResponseEntity<ExceptionResponse>(res, HttpStatus.BAD_REQUEST);
+	}
+
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ExceptionResponse> handleValidationException(MethodArgumentNotValidException ex) {
+
+		List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+		List<String> errors = fieldErrors.stream()
+				.map(error -> error.getField() + ": " + error.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		ExceptionResponse res = new ExceptionResponse(new Date(),"method argument not valid", errors);
+		return new ResponseEntity<ExceptionResponse>(res, HttpStatus.BAD_REQUEST);
+	}
+
+
+	@ExceptionHandler(ResponseStatusException.class)
+	public ResponseEntity<ExceptionResponse> handleResponseStatusException(ResponseStatusException ex) {
+
+		ExceptionResponse res = new ExceptionResponse(new Date(),"there is no user with that id", ex.getMessage());
+
+		return new ResponseEntity<ExceptionResponse>(res, HttpStatus.NOT_FOUND);
+	}
+
 }
