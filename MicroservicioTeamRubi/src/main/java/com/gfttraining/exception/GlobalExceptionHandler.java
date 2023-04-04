cf308ae8836f27d3a19744536b99afaea18c6d2c
Sergi Perez <sergi.perez@gft.com>
2023-04-04 08:39:49 +0000

    customized exceptions for email validation and new data.sql

--- MicroservicioTeamRubi/src/main/java/com/gfttraining/exception/GlobalExceptionHandler.java
+++ MicroservicioTeamRubi/src/main/java/com/gfttraining/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.gfttraining.exception;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -65,5 +67,16 @@ public class GlobalExceptionHandler {
 
 		return new ResponseEntity<ExceptionResponse>(res, HttpStatus.NOT_FOUND);
 	}
+
+
+	@ExceptionHandler(DuplicateEmailException.class)
+	public ResponseEntity<ExceptionResponse> handleDataIntegrityViolationException(DuplicateEmailException ex) {
+
+		ExceptionResponse res = new ExceptionResponse(ex.getMessage(), new Date());
+
+		return new ResponseEntity<ExceptionResponse>(res, HttpStatus.CONFLICT);
+	}
+
+
 
 }
